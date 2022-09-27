d6855088cb1dc4b2cb6e5c81a0130c7b0aded395
Umair Akram <umair.akram687@gmail.com>
2022-09-27 14:33:14 +0500

    MyDigitalWatch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const time = new Date().toLocaleTimeString();
+  const [ctime, setCTime] = useState(time);
+  const UpdateTime = () => {
+    const time = new Date().toLocaleTimeString();
+    setCTime(time);
+  };
+  setInterval(UpdateTime, 1000);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>{ctime}</h3>
     </div>
   );
 }
